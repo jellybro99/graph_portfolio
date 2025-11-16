9f4e9040d639cf05de0b167a1ce7cbdcc4e7f9e1
jellybro99 <benluke1111@gmail.com>
2025-11-15 22:23:15 -0600

    Changed to a grid layout for main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,17 @@ import ProjectList from './components/ProjectList.tsx';
 
 export default function App() {
   return (
-    <div className='flex flex-col min-h-screen w-full'>
-      <div className='h-screen'>
-        <header>
+    <div className='min-h-screen w-full'>
+      <div className='h-screen grid grid-cols-2 gap-2 p-2'>
+        <header className='col-span-2 border-2'>
           <InfoHeader />
         </header>
-        <div className='flex flex-row'>
-          <section className='basis-auto w-full'>
-            <ProjectList />
-          </section>
-          <section className='basis-auto w-full'>
-            <Graph />
-          </section>
-        </div>
+        <section className='min-h-0 w-full border-2'>
+          <ProjectList />
+        </section>
+        <section className='min-h-0 w-full border-2'>
+          <Graph />
+        </section>
       </div>
       <div className='h-screen'>
         more stuff
