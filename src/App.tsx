5d34e9f12a68bbeb90b3341cb5d7475b9099d27b
jellybro99 <benluke1111@gmail.com>
2025-11-16 12:52:54 -0600

    some god aweful styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import ProjectList from './components/ProjectList.tsx';
 
 export default function App() {
   return (
-    <div className='min-h-screen w-full'>
-      <div className='h-screen grid grid-cols-2 gap-2 p-2'>
-        <header className='col-span-2 border-2'>
+    <div className='min-h-screen w-full text-primary'>
+      <div className='h-screen grid grid-cols-2 gap-2 p-2 bg-background'>
+        <header className='col-span-2 border border-primary bg-foreground'>
           <InfoHeader />
         </header>
-        <section className='min-h-0 w-full border-2'>
+        <section className='min-h-0 w-full border border-primary bg-foreground'>
           <ProjectList />
         </section>
-        <section className='min-h-0 w-full border-2'>
+        <section className='min-h-0 w-full border border-primary bg-foreground'>
           <Graph />
         </section>
       </div>
