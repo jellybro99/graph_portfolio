8a82bb8f64c3479a2df4db9435ce7bc05bcf5116
jellybro99 <benluke1111@gmail.com>
2025-11-23 21:22:16 -0600

    Fixed filler variable name

--- src/utils/usePrefersDarkMode.ts
+++ src/utils/usePrefersDarkMode.ts
@@ -7,12 +7,12 @@ export default function usePrefersDarkMode() {
     if (matcher.matches) document.documentElement.classList.add("dark");
     else document.documentElement.classList.remove("dark");
 
-    const blob = (event: MediaQueryListEvent) => {
+    const themeHandler = (event: MediaQueryListEvent) => {
       if (event.matches) document.documentElement.classList.add("dark");
       else document.documentElement.classList.remove("dark");
     };
 
-    matcher.addEventListener("change", blob);
-    return () => matcher.removeEventListener("change", blob);
+    matcher.addEventListener("change", themeHandler);
+    return () => matcher.removeEventListener("change", themeHandler);
   }, []);
 }
