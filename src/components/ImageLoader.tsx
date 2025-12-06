77b61fe8dd46dc70653154dcca6005440c160de3
jellybro99 <benluke1111@gmail.com>
2025-12-06 16:57:07 -0600

    Preview image done for real this

--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -14,19 +14,21 @@ export default function ProgressiveImage({
   const [loaded, setLoaded] = useState(false);
 
   return (
-    <>
+    <div className="relative">
       {!loaded && (
-        <img
-          src={placeholder}
-          className="w-full h-full aspect-video"
-        />
+        <img src={placeholder} className="absolute w-full aspect-video" />
       )}
       <img
         src={src}
         onClick={onClick}
         onLoad={() => setLoaded(true)}
-        className={className + " " + (loaded ? "opacity-100" : "opacity-0") + " transition-opacity duration-300"}
+        className={
+          className +
+          " " +
+          (loaded ? "opacity-100" : "opacity-0") +
+          " transition-opacity duration-500"
+        }
       />
-    </>
+    </div>
   );
 }
