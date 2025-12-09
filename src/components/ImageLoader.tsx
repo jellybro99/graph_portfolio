93875e61a99c3b1c6ac54bbaac545509ac8c6794
jellybro99 <benluke1111@gmail.com>
2025-12-08 20:30:39 -0600

    temporary fix until I add the aspect ratio to the image

--- src/components/ImageLoader.tsx
+++ src/components/ImageLoader.tsx
@@ -14,7 +14,7 @@ export default function ImageLoader({
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <div className="relative w-full">
+    <div className="relative w-full aspect-video">
       <img
         src={placeholder}
         className={
